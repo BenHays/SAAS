e1871d0923c3ecdaaa40ced36e59a4478cb594a8
Ben Hays <benjaminjameshays@gmail.com>
2016-12-24 13:42:32 +0000

    fixed error with About and Contact us page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+    #whitelist the following form fields so that we can process them, if coming from
+    # devise form field
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   
 protected
